23137e444d6e7c9c0a8d8fb371b3b7d7d3622c87
dinhdiepvuong <diepvuong.py@gmail.com>
2023-05-22 22:44:12 +0700

    docs update employee

---
+++ src/docs/basicInfo.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    openapi: '3.0.0',
+    info: {
+        version: '1.0.0',
+        title: ' App API',
+        description: 'API Swagger Quan Ly Nhà Hàng App',
+        license: {
+            name: 'Terms of service',
+            url: 'http://swagger.io/terms'
+        },
+        contact: {
+            name: 'Thong App',
+            email: 'nthung0807@gmail.com'
+        }
+    }
+};
